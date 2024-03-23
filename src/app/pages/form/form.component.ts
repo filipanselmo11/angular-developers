8defcf3bb84b151b4594822d9da64c8f9002b1c0
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-03-23 16:35:23 -0400

    feat: Componente de formulário, OK

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,39 +1,59 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { DevService } from '../../services/dev.service';
 import { Router } from '@angular/router';
+import { DevInterface } from '../../types/dev';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule],
+  providers: [ DevService ],
+  imports: [MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
-  id: number = Math.floor(Math.random() * 1000) + 1;
-  name: string =   '';
-  email: string = '';
-  technologies: string = '';
+  devForm!: FormGroup;
 
-  constructor(private devService: DevService, private router: Router){}
+  constructor(private devService: DevService, private router: Router){
+    // Método 1
+    // private fb: FormBuilder
+    // this.devForm = this.fb.group({
+    //   name: ['', Validators.required],
+    //   email: ['', Validators.required, Validators.email],
+    //   technologies: ['', Validators.required],
+    // });
+
+    // Método 2
+    this.devForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      technologies: new FormControl('', [Validators.required]),
+    });
+
+    // this.data = {
+    //   id: Math.floor(Math.random() * 1000) + 1,
+    //   name: this.devForm.value.name,
+    //   email: this.devForm.value.email,
+    //   technologies: this.devForm.value.technologies
+    // }
+  }
 
   submitForm() {
-    const dados = {
-      id: this.id,
-      name: this.name,
-      email: this.email,
-      technologies: this.technologies,
+    const data: DevInterface = {
+      id: Math.floor(Math.random() * 1000) + 1,
+      name: this.devForm.value.name,
+      email: this.devForm.value.email,
+      technologies: this.devForm.value.technologies
     }
-
-    this.devService.createDev(dados).subscribe(res => {
+    this.devService.createDev(data).subscribe((res)=> {
       console.log('RES ', res);
-      this.router.navigateByUrl('');
-    }, error => {
-      console.log('ERROR ', error);
+      this.router.navigate(['']);
+    }, (error) => {
+      console.error('ERROR ', error);
     });
   }
 }
